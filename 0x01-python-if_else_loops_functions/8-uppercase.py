b197af2bbeb6692ab0e7b7d6423d2e0f64e6a2ca
geraldrolland <geraldrolland123@gmail.com>
2023-11-01 16:11:27 -0700

    mytask 8

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -4,3 +4,8 @@ def uppercase(str):
         for j in range(97, 123):
             if i == chr(j):
                 print("{}".format(chr(j - 32)), end="")
+		break
+	    if j == 122:
+	        print(i, end="")
+		break
+	    continue
