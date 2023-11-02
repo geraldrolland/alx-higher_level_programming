4d2aaed23370d28a9c3933ae9ec637d1698534e1
geraldrolland <geraldrolland123@gmail.com>
2023-11-02 16:08:12 -0700

    my task 2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+j = 1
+if len(sys.argv) == 1:
+    print("0 arguments.")
+else:
+    print("{} arguments:".format(len(sys.argv) - 1))
+    while j < len(sys.argv):
+	    print("{}: {}".format(j, sys.argv[j]))
+	    j = j + 1
+
+
