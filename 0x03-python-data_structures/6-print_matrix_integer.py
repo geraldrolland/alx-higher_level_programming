529475d1a1732b0ea8145f12726f2ddf2677d1de
geraldrolland <geraldrolland123@gmail.com>
2023-11-06 04:12:11 -0800

    my task 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,14 +1,6 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if matrix == [[]]:
-        print()
-        return
     for lst in matrix:
-        last_elem = lst[-1]
         for elem in lst:
-            txt = "{}"
-            if last_elem == elem:
-                print(txt.format(elem), end="")
-                break
-            print(txt.format(elem), end=" ")
+            print("{}".format(elem), end=" " if elem != lst[-1] else "")
         print()
