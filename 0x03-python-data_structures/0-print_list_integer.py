d316069b59b86240389db566ed5921bffd711fcd
geraldrolland <geraldrolland123@gmail.com>
2023-11-06 00:23:37 -0800

    my task 0

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -3,4 +3,5 @@ def print_list_integer(my_list=[]):
     if my_list == []:
         print()
     for elem in my_list:
-        print("{}".format(elem))
+        txt = "{}"
+        print(txt.format(elem))
