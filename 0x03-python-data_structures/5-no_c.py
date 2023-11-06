096d5dbbab98e53f6d6fd8365bf2e05f5a4e289b
geraldrolland <geraldrolland123@gmail.com>
2023-11-06 03:28:07 -0800

    my task 5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -3,7 +3,7 @@ def no_c(my_string):
     j = 0
     for i in my_string:
         if i == "C" or i == "c":
-            my_string = my_string[:j] + my_string[j+1:]
+            my_string = my_string[:j] + my_string[j + 1:]
             continue
         j += 1
     return my_string
