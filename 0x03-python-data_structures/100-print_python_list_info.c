f25dd9c7e571c10ae6281cbb2fc8f7a778b149c2
geraldrolland <geraldrolland123@gmail.com>
2023-11-06 16:31:33 -0800

    my task [2~

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -12,5 +12,7 @@ void print_python_list_info(PyObject *p)
 	printf("[*] Size of the Python List = %lu\n", Py_SIZE(p));
 	printf("[*] Allocated = %lu\n", ((PyListObject *)p)->allocated);
 	for (elem = 0; elem < Py_SIZE(p); elem++)
-		printf("Element %d: %s\n", elem, Py_TYPE(PyList_GetItem(p, elem))->tp_name);
+	{
+	printf("Element %d: %s\n", elem, Py_TYPE(PyList_GetItem(p, elem))->tp_name);
+	}
 }
