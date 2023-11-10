3affa0b38d3915ed347a4176cdd11a5d27eebddd
geraldrolland <geraldrolland123@gmail.com>
2023-11-10 15:32:15 -0800

    my task 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary == None:
+    if a_dictionary is None:
         return "None"
     value_list = a_dictionary.values()
     for value in value_list:
