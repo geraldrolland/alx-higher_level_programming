fa6085ead04dbe19bab9915c87968c049dbd61f4
geraldrolland <geraldrolland123@gmail.com>
2023-11-30 06:31:22 -0800

    my task 4

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,7 +1,18 @@
 #!/usr/bin/python3
 
 
+"""
+This is the docstring for module 4-print_square
+
+This module provide the function print_square(...)
+"""
+
+
 def print_square(size):
+    """Print the size of square using #.
+    args:
+	size(int): size of the square
+    """
     if (isinstance(size, float) and size < 0) or size is None or \
             not isinstance(size, int):
         raise TypeError("size must be an integer")
