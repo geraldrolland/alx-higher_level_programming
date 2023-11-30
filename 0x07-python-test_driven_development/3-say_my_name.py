e274d2937e57646658a6697d3596660400653d85
geraldrolland <geraldrolland123@gmail.com>
2023-11-30 06:43:35 -0800

    my task 3

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,8 +1,19 @@
 #!/usr/bin/python3
 
 
+"""
+This is module docstring for 3-say_my_name
+
+This module provide the function say_my_name(..)
+"""
+
+
 def say_my_name(first_name, last_name=""):
-    """Print the first name and last name"""
+    """Print the first name and last name.
+    args:
+        first_name(str): first name
+        last_name(str): last name
+    """
     if type(first_name) not in [str] or \
             first_name is None or first_name == "":
         raise TypeError("first_name must be a string")
@@ -12,9 +23,6 @@ def say_my_name(first_name, last_name=""):
         if char.isdigit() is True:
             raise TypeError("first_name must be a string")
         continue
-    if last_name == "":
-        print("My name is {}".format(first_name))
-        return
     for char in last_name:
         if char.isdigit() is True:
             raise TypeError("last_name must be a string")
