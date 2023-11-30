4dbca0244eb2e3e85c12cb74114be395cca90cf4
geraldrolland <geraldrolland123@gmail.com>
2023-11-30 07:20:31 -0800

    my task 0

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,8 +1,19 @@
 #!/usr/bin/python3
 
 
+"""
+This is the docstring for module 0-add_integer
+
+This module provide the function add_integer(...)
+"""
+
+
 def add_integer(a, b=98):
-    """""Return the addition of a and b"""
+    """""Return the addition of a and b.
+    args:
+        a(int): a
+        b(int): b
+    """""
 
     if not isinstance(a, int) and not isinstance(a, float) or a is None:
         raise TypeError("a must be an integer")
