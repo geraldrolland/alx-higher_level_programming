094863a1cd1d782fa31eec9cac2166714ad02509
geraldrolland <geraldrolland123@gmail.com>
2023-12-04 16:20:47 -0800

    my task 1

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+
+"""
+This module defines the class MyList
+"""
+
+class MyList(list):
+    """Define a class MyList"""
+    def print_sorted(self):
+        """that prints the list, but sorted.
+        args:
+            no argument
+        """
+        my_sort = sorted(self)
+        print(my_sort)
