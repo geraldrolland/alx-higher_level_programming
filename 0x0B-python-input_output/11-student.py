b0d58e501c928c1bda9382712e4442e71a5cdcfa
geraldrolland <geraldrolland123@gmail.com>
2023-12-06 04:47:57 -0800

    my task 11

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+
+
+"""
+This module defines the class Student
+"""
+
+
+class Student:
+
+    """Define class student"""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialize the attribute of the object.
+        args:
+            first_name: first name
+            last_name: last name
+            age: age
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Return dictionary"""
+        if attrs is None:
+            return self.__dict__
+        new_dict = {}
+        my_dict_copy = self.__dict__.copy()
+        key_list = my_dict_copy.keys()
+        for key in attrs:
+            if key in key_list:
+                value = my_dict_copy[key]
+                new_dict.update({key: value})
+            continue
+        return new_dict
+
+    def reload_from_json(self, json):
+        """replaces all attributes of the Student instance"""
+        self.__dict__ = json
+        return self.__dict__
