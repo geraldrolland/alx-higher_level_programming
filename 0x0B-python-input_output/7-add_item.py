d3bf27ec59c365728eb6acfbada31cff86eb9df2
geraldrolland <geraldrolland123@gmail.com>
2023-12-05 19:53:10 -0800

    my task 7

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+
+"""
+This module provide a programme
+"""
+
+
+import sys
+
+
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+
+try:
+    file = load_from_json_file("add_item.json")
+except FileNotFoundError:
+    save_to_json_file([], "add_item.json")
+else:
+    if len(sys.argv) > 1 and file != "":
+        i = 1
+        while i < len(sys.argv):
+            file.append(sys.argv[i])
+            i += 1
+        save_to_json_file(file, "add_item.json")
