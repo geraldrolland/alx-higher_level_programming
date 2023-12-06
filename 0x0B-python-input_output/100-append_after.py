3e9667195dee5c3d9262c8470d7d9543b0c1fb06
geraldrolland <geraldrolland123@gmail.com>
2023-12-06 13:45:00 -0800

    my task 100

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+
+
+"""
+This module provide the function append_after(...)
+"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """inserts a line of text to a file,.
+    args:
+        filename(str): file name
+        search_string(str): search string
+        new_string(str): new string to insert
+    """
+    try:
+        with open(filename, "r+", encoding="UTF8") as file:
+            lines = file.readlines()
+            if lines == []:
+                file.write(new_string)
+                return
+            my_list = []
+            for line in lines:
+                my_list.append(line)
+        with open(filename, "w", encoding="UTF8") as file:
+            for lines in my_list:
+                file.write(lines)
+                line_split = lines.split()
+                for word in line_split:
+                    if search_string in word:
+                        file.write(new_string)
+                    continue
+                continue
+    except FileNotFoundError:
+        with open(filename, "w", encoding="UTF8") as file:
+            file.write(new_string)
