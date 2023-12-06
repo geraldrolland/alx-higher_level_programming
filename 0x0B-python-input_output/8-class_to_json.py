88856faa2e761469d35b5e1430608dd2104dddb6
geraldrolland <geraldrolland123@gmail.com>
2023-12-05 21:13:06 -0800

    my task 8

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -8,5 +8,4 @@ This module define function class_to_json(...)
 
 def class_to_json(obj):
     """convert class object to jason"""
-    my_str = json.dumps(obj.__dict__)
-    return json.loads(my_str)
+    return obj.__dict__
