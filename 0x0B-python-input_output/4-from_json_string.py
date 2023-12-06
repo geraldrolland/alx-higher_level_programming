d099f47900044de6406b44fc77d3f6afd4681cf9
geraldrolland <geraldrolland123@gmail.com>
2023-12-05 18:59:29 -0800

    my task 4

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+
+"""
+This module defines a function from_json_string(my_str)
+"""
+
+
+import json
+
+
+
+def from_json_string(my_str):
+    """ returns an object  represented by a JSON string.
+    args:
+        my_str: jason object
+    """
+    return json.loads(my_str)
