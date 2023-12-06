c4c9b0ea4a7c0854ce6a761679ca8eae19c7465b
geraldrolland <geraldrolland123@gmail.com>
2023-12-06 03:29:16 -0800

    my task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -18,8 +18,9 @@ try:
     file = load_from_json_file("add_item.json")
 except FileNotFoundError:
     save_to_json_file([], "add_item.json")
-else:
-    if len(sys.argv) > 1 and file != "":
+finally:
+    file = load_from_json_file("add_item.json")
+    if len(sys.argv) > 1:
         i = 1
         while i < len(sys.argv):
             file.append(sys.argv[i])
