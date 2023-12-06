2b5ace16877a3f7df78e5f99d730d4a734a8508a
geraldrolland <geraldrolland123@gmail.com>
2023-12-05 21:02:17 -0800

    my task 10

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -16,9 +16,9 @@ class Student:
     def __init__(self, first_name, last_name, age):
         """Initialize the attribute of the object.
         args:
-            first_name : first name
-            last_name : last name
-            age : age
+            first_name: first name
+            last_name: last name
+            age: age
         """
         self.first_name = first_name
         self.last_name = last_name
