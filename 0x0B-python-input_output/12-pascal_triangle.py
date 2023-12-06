06c88f7f7ec9ce4b4fef6709d17c7581972ab095
geraldrolland <geraldrolland123@gmail.com>
2023-12-06 06:03:34 -0800

    my task 12

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+
+
+"""
+This module defines a function pascal_triangle(n)
+"""
+
+
+def pascal_triangle(n):
+    """Return a pascal triangle
+    args:
+        n(int): number of triangles
+    """
+    if n <= 0:
+        return [[]]
+    if n == 1:
+        return [[1]]
+    if n == 2:
+        return [[1], [1,1]]
+    my_list =[[1],[1,1]]
+    i = 2
+    while i < n:
+        sub_list = []
+        sub_list.append(1)
+        j = 0
+        sum = 0
+        while  j + 1 < len(my_list[-1]):
+            sum += my_list[-1][j] + my_list[-1][j + 1]
+            sub_list.append(sum)
+            sum = 0
+            j += 1
+        sub_list.append(1)
+        my_list.append(sub_list)
+        i += 1
+    return my_list
