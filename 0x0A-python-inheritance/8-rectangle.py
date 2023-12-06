f5993081933b99466f77d2080c9e2cc93c4a336c
geraldrolland <geraldrolland123@gmail.com>
2023-12-06 15:47:30 -0800

    my task 10

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+
+"""
+This module defines the class Rectangle
+"""
+
+
+class Rectangle(BaseGeometry):
+    
+    """define a class Rectangle"""
+    
+    def __init__(self, width, height):
+        """Initialize the attribute of the object
+        args:
+            width(int): width of the rectangle
+            height(int): height of the rectangle
+        """
+        self.__width = width
+        self.__height = height
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+    def integer_validator(self, name, value):
+        """Validate an integer"""
+        super().integer_validator(name, value)
