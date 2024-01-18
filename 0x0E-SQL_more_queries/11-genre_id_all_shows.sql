9a6cdf06ee745694bff39899f710bb926ee47383
geraldrolland <geraldrolland123@gmail.com>
2024-01-18 08:59:19 -0800

    my task 0

---
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,1 @@
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows LEFT JOIN tv_show_genres ON tv_shows.ID = tv_show_genres.show_id ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
