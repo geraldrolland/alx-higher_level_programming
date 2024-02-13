e26e3014b5f451e6bf52ad96d0d95fd95918f1d9
geraldrolland <geraldrolland123@gmail.com>
2024-02-13 04:05:47 -0800

    my task

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor(w, h) {
+    if ((h > 0) && (w > 0)) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+  print() {
+    for (; this.height >= 1; this.height--) {
+      let x = 'X';
+      console.log(x.repeat(this.width));
+    }
+  }
+}
+module.exports = Rectangle;
---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+class Rectangle {
+    constructor(w, h) {
+      if ((h > 0) && (w > 0)) {
+        this.width = w;
+        this.height = h;
+      }
+    }
+    print() {
+      for (; this.height >= 1; this.height--) {
+        let x = 'X';
+        console.log(x.repeat(this.width));
+      }
+    }
+    rotate() {
+      const temp = this.width;
+      this.width = this.height;
+      this.height = temp;
+    }
+    double() {
+      this.width = 2 * this.width;
+      this.height = 2 * this.height;
+    }
+  }
+  module.exports = Rectangle;
