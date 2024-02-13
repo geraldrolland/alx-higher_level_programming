077f1ccdcb94543c55d3297afb51ee4bb4e0cc41
geraldrolland <geraldrolland123@gmail.com>
2024-02-13 09:12:40 -0800

    my task

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-var i = 0
+let i = 0;
 const list = require('./100-data.js').list;
-let newList = list.map((arg) => {return arg * (i++)});
+const newList = list.map((arg) => { return arg * (i++); });
 console.log(list);
 console.log(newList);
 i = 0;
