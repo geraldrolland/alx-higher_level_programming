fbd7be73da5c8b977c93d649e4eebfabb538c44a
geraldrolland <geraldrolland123@gmail.com>
2024-02-13 04:31:33 -0800

    my task

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,25 +1,28 @@
 #!/usr/bin/node
 class Rectangle {
-    constructor (w, h) {
-      if ((h > 0) && (w > 0)) {
-        this.width = w;
-        this.height = h;
+  constructor (w, h) {
+    if ((h > 0) && (w > 0)) {
+      this.width = w;
+      this.height = h;
     }
   }
-    print() {
-      for (; this.height >= 1; this.height--) {
-        let x = 'X';
-        console.log(x.repeat(this.width));
+
+  print () {
+    for (let i = this.height; i >= 1; i--) {
+      const x = 'X';
+      console.log(x.repeat(this.width));
     }
   }
-    rotate() {
-      const temp = this.width;
-      this.width = this.height;
-      this.height = temp;
+
+  rotate () {
+    const temp = this.width;
+    this.width = this.height;
+    this.height = temp;
   }
-    double() {
-      this.width = 2 * this.width;
-      this.height = 2 * this.height;
+
+  double () {
+    this.width = 2 * this.width;
+    this.height = 2 * this.height;
   }
 }
 
