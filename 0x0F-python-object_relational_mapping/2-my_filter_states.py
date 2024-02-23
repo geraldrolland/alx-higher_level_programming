7b86ca40399e156a9457eb3c87ab543a7254f8b5
geraldrolland <geraldrolland123@gmail.com>
2024-02-23 07:38:26 -0800

    orm

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -8,20 +8,20 @@ the states table of hbtn_0e_0_usa where name matches the argument.
 if __name__ == "__main__":
     import MySQLdb
     import sys
-db_uri = {
-    "host": "localhost",
-    "port": 3306,
-    "user": sys.argv[1],
-    "passwd": sys.argv[2],
-    "db": sys.argv[3],
-    "charset": "utf8"
-    }
-conn = MySQLdb.connect(**db_uri)
-cur = conn.cursor()
-query = "SELECT id, name FROM states WHERE name = '{}'".format(sys.argv[4])
-cur.execute(query)
-state_list = cur.fetchall()
-for state in state_list:
-    print("({} '{}')".format(state[0], state[1]))
-cur.close()
-conn.close()
+    db_uri = {
+        "host": "localhost",
+        "port": 3306,
+        "user": sys.argv[1],
+        "passwd": sys.argv[2],
+        "db": sys.argv[3],
+        "charset": "utf8"
+       }
+    conn = MySQLdb.connect(**db_uri)
+    cur = conn.cursor()
+    query = "SELECT * FROM states WHERE name = '{}'".format(sys.argv[4])
+    cur.execute(query)
+    state_list = cur.fetchall()
+    for state in state_list:
+        print(state)
+    cur.close()
+    conn.close()
