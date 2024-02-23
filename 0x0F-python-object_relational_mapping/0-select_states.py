ab7d79d203bb23c0e11f5f9ea18a8f169b2c823d
geraldrolland <geraldrolland123@gmail.com>
2024-02-23 07:11:05 -0800

    orm

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -9,32 +9,29 @@ if __name__ == "__main__":
     import MySQLdb
     import sys
     # Checking if the required command-line arguments are provided
-    if len(sys.argv) != 4:
-        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
-        sys.exit(1)
+    if len(sys.argv) == 4:
+        # Setting up database connection parameters
+        db_uri = {
+            "host": "localhost",
+            "port": 3306,
+            "user": sys.argv[1],
+            "passwd": sys.argv[2],
+            "db": sys.argv[3],
+            "charset": "utf8"
+        }
 
-    # Setting up database connection parameters
-    db_uri = {
-        "host": "localhost",
-        "port": 3306,
-        "user": sys.argv[1],
-        "passwd": sys.argv[2],
-        "db": sys.argv[3],
-        "charset": "utf8"
-    }
+        # Connecting to the MySQL database
+        conn = MySQLdb.connect(**db_uri)
+        cur = conn.cursor()
 
-    # Connecting to the MySQL database
-    conn = MySQLdb.connect(**db_uri)
-    cur = conn.cursor()
+        # Executing the SQL query to fetch states
+        cur.execute("SELECT id, name FROM states ORDER BY id ASC")
+        state_list = cur.fetchall()
 
-    # Executing the SQL query to fetch states
-    cur.execute("SELECT id, name FROM states ORDER BY id ASC")
-    state_list = cur.fetchall()
+        # Printing the fetched states
+        for state in state_list:
+            print("({} '{}')".format(state[0], state[1]))
 
-    # Printing the fetched states
-    for state in state_list:
-        print("({} '{}')".format(state[0], state[1]))
-
-    # Closing cursor and connection
-    cur.close()
-    conn.close()
+        # Closing cursor and connection
+        cur.close()
+        conn.close()
