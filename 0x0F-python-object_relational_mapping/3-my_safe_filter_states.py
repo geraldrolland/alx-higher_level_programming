1ac1456bb878b6e61c20fa601211cfef69569255
geraldrolland <geraldrolland123@gmail.com>
2024-02-23 20:32:26 -0800

    orm

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -8,27 +8,27 @@ including their id
 
 if __name__ == "__main__":
     import MySQLdb
-    import sys
-db_uri = {
-    "host": "localhost",
-    "port": 3306,
-    "user": sys.argv[1],
-    "passwd": sys.argv[2],
-    "db": sys.argv[3],
-    "charset": "utf8"
-    }
-try:
-    conn = MySQLdb.connect(**db_uri)
-except Exception as e:
-    print(e)
-cur = conn.cursor()
-query = "SELECT states.id, states.name FROM states WHERE states.name = %s"
-try:
-    cur.execute(query, (sys.argv[4],))
-except Exception as e:
-    print(e)
-state_list = cur.fetchall()
-for state in state_list:
-    print("({} '{}')".format(state[0], state[1]))
-cur.close()
-conn.close()
+    from sys import argv
+    db_uri = {
+        "host": "localhost",
+        "port": 3306,
+        "user": argv[1],
+        "passwd": argv[2],
+        "db": argv[3],
+        "charset": "utf8"
+        }
+    try:
+        conn = MySQLdb.connect(**db_uri)
+    except Exception as e:
+        print(e)
+    cur = conn.cursor()
+    query = "SELECT states.id, states.name FROM states WHERE states.name = %s"
+    try:
+        cur.execute(query, (argv[4],))
+    except Exception as e:
+        print(e)
+    state_list = cur.fetchall()
+    for state in state_list:
+        print(state)
+    cur.close()
+    conn.close()
