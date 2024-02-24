0012fdb3f4d999b841b6afff43aa977a62546861
geraldrolland <geraldrolland123@gmail.com>
2024-02-23 20:56:38 -0800

    orm

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,19 +1,24 @@
 #!/usr/bin/python3
 
+"""
+This script takes in the name of a state as an argument and lists all cities
+"""
+
+
 if __name__ == "__main__":
     import MySQLdb
-    import sys
+    from sys import argv
 
     conn = MySQLdb.connect(host="localhost",
                            port=3306,
-                           user="root",
-                           passwd="root",
-                           db="hbtn_0e_4_usa",
+                           user=argv[1],
+                           passwd=argv[2],
+                           db=argv[3],
                            charset="utf8")
     cur = conn.cursor()
     query = " ON states.id=cities.state_id WHERE states.name = %s;"
     query = "SELECT cities.name FROM states JOIN cities" + query
-    cur.execute(query, (sys.argv[4],))
+    cur.execute(query, (argv[4],))
     my_list = cur.fetchall()
     city = ""
     size = len(my_list)
