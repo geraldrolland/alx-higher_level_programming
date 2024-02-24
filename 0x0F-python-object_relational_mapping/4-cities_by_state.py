c0ff5eb2ab2d653475003f41f862abfee2d662fd
geraldrolland <geraldrolland123@gmail.com>
2024-02-23 20:37:58 -0800

    orm

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,24 +1,25 @@
 #!/usr/bin/python3
+
 if __name__ == "__main__":
     import MySQLdb
-    import sys
-db_uri = {
-    "host": "localhost",
-    "port": 3306,
-    "user": sys.argv[1],
-    "passwd": sys.argv[2],
-    "db": sys.argv[3],
-    "charset": "utf8"
-    }
-conn = MySQLdb.connect(**db_uri)
-cur = conn.cursor()
-query = " states.name, cities.name ORDER BY states.id ASC;"
-query = "states.id=cities.state_id GROUP BY states.id," + query
-q1 = "SELECT states.id, cities.name, states.name FROM states JOIN cities ON "
-query = q1 + query
-cur.execute(query)
-my_list = cur.fetchall()
-for row in my_list:
-    print("({} '{}' '{}')".format(row[0], row[1], row[2]))
-cur.close()
-conn.close()
+    from sys import argv
+    db_uri = {
+        "host": "localhost",
+        "port": 3306,
+        "user": argv[1],
+        "passwd": argv[2],
+        "db": argv[3],
+        "charset": "utf8"
+        }
+    conn = MySQLdb.connect(**db_uri)
+    cur = conn.cursor()
+    query = " states.name, cities.name ORDER BY states.id ASC;"
+    query = " states.id=cities.state_id GROUP BY states.id," + query
+    q = "SELECT states.id, cities.name, states.name FROM states JOIN cities ON"
+    query = q + query
+    cur.execute(query)
+    my_list = cur.fetchall()
+    for row in my_list:
+        print(row)
+    cur.close()
+    conn.close()
