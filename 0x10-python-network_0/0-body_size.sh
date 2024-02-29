118ab2b872c828bd0ee755ad7ef046cc806fa7c1
geraldrolland <geraldrolland123@gmail.com>
2024-02-29 08:06:35 -0800

    python network

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,9 +1,3 @@
 #!/usr/bin/env bash
 #This script that takes in a URL, sends a request to that URL
-#and displays the size of the body of the response
-
-if [[ "$#" -eq 1 ]]; then
-    url=$1
-    content_length=$(curl -s "$url" | wc -c)
-    echo "$content_length"
-fi
+echo $(curl -s "$1" | wc -c)
