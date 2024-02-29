c6ad86337ad3e278dde807fb53e72abc108bd864
geraldrolland <geraldrolland123@gmail.com>
2024-02-29 14:30:03 -0800

    python network

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash
 #This script that takes in a URL, sends a request to that URL
 echo "$(curl -s "$1" | wc -c)"
 
--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,14 +1,4 @@
 #!/usr/bin/env bash
-#This script takes in a URL, sends a GET request to the URL and displays the body of the response
-if [[ "$#" -eq 2 ]]; then
-    url=$1
-    response=$(curl -s -o temp_body.txt -w "%{http_code}" "$url")
-    status_code=$(tail -n1 <<< "$response")
-    
-    if [[ $status_code -eq 200 ]]; then
-        cat temp_body.txt
-	rm temp_body.txt
-    else
-        rm temp_body.txt
-    fi
-fi
+#This script takes in a URL, sends a GET request to the URL, and displays the body of the response
+url=$1; [[ $(curl -s -o temp_body.txt -w "%{http_code}" "$url") -eq 200 ]] && cat temp_body.txt; rm -f temp_body.txt
+
