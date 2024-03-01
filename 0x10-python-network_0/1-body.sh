5baf9cc0defe2eab0243973005e75ecd9e6b34e3
geraldrolland <geraldrolland123@gmail.com>
2024-03-01 04:31:45 -0800

    python network

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #This script takes in a URL, sends a GET request to the URL, and displays the body of the response
-url=$1; [[ $(curl -s -X GET -o temp_body.txt -w "%{http_code}" "$url") -eq 200 ]] && cat temp_body.txt; rm -f temp_body.txt
+url=$1; [[ $(curl -s -L -X GET -o temp_body.txt -w "%{http_code}" "$url") -eq 200 ]] && cat temp_body.txt; rm -f temp_body.txt
