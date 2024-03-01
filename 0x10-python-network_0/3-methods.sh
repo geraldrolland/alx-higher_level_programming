7ce446987f44366e0cdf7fec6d3e5f2e64abfb7e
geraldrolland <geraldrolland123@gmail.com>
2024-03-01 04:33:46 -0800

    python network

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script that takes in a URL and displays all HTTP methods the server
-echo $(grep -i "Allow" <<< $(curl -s -X OPTIONS -i "$1")) | sed 's/Allow: //'
+echo $(grep -i "Allow" <<< $(curl -s -L -X OPTIONS -i "$1")) | sed 's/Allow: //'
