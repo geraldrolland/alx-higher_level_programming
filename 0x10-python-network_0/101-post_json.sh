dbf6fc50834e99b7d0fe74226767a216ef36afa6
geraldrolland <geraldrolland123@gmail.com>
2024-03-01 04:57:24 -0800

    python network

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script sends a JSON POST request to a URL passed as the first argument, and displays the body of the response.
-echo $(curl -s -d @"$2" -X POST "$1")
+echo $(curl -s -L -d @"$2" -X POST "$1")
