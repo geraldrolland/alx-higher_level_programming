3dc1a9be1a97c23cb22eb1638fef2de10366aa34
geraldrolland <geraldrolland123@gmail.com>
2024-03-03 08:47:23 -0800

    python network 1

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
 """
+Thsi script takes in a letter and sends a POST request to
+http://0.0.0.0:5000/search_user with the letter as a parameter.
 """
 
 if __name__ == "__main__":
