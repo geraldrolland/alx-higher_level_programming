23ab52a86ff5ccdbc5201115ff6b4aba91481e41
geraldrolland <geraldrolland123@gmail.com>
2024-03-05 12:18:34 -0800

    python network 0

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/bash
+#!/bin/bash
 # This script makes a request to 0.0.0.0:5000/catch_me that causes
 # the server to respond with a message containing You got me!, in the body of the response
 curl -s -X PUT 0.0.0.0:5000/catch_me -d "user_id=98" -H "origin: HolbertonSchool"
