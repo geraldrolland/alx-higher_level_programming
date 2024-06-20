b98bc45d98c6b9234bfd236ea1342ec2c29e831e
geraldrolland <geraldrolland123@gmail.com>
2024-06-19 18:39:11 -0700

    javascript

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -5,6 +5,6 @@ if (process.argv.length === 2) {
   if (isNaN(parseInt(process.argv[2]))) {
     console.log('Not a number');
   } else {
-    console.log("My number: " + parseInt(process.argv[2]));
+    console.log('My number: ' + parseInt(process.argv[2]));
   }
 }
