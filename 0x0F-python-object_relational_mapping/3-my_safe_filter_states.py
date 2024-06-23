e8e4539b3f36b9ab12b0348fb0ac52e5c8fd43f1
geraldrolland <geraldrolland123@gmail.com>
2024-06-23 05:31:20 -0700

    orm

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
 
+"""
+This scripts list state recorde that matches the
+fourth commandline argument and prevent sql injection
+"""
+
 if __name__ == "__main__":
     import MySQLdb
     import sys
