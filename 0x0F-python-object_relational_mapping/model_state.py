e7df3d0a65d67b76bae7722a3d3291c242b3184f
geraldrolland <geraldrolland123@gmail.com>
2024-06-23 05:58:22 -0700

    orm

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
+
 """
-Thi
+This script create a table states
 """
+
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, String, Integer
 Base = declarative_base()
