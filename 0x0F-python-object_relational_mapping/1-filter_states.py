c6398607195358673265233892683fd22f7b7af6
geraldrolland <geraldrolland123@gmail.com>
2024-06-23 05:15:33 -0700

    orm

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -8,14 +8,14 @@ if __name__ == "__main__":
     import sys
     import MySQLdb
 
-     conn = MySQLdb.connect(
-            host="localhost",
-            port=3306,
-            user=sys.argv[1],
-            passwd=sys.argv[2],
-            db=sys.argv[3],
-            charset="utf8"
-            )
+    conn = MySQLdb.connect(
+         host="localhost",
+         port=3306,
+         user=sys.argv[1],
+         passwd=sys.argv[2],
+         db=sys.argv[3],
+         charset="utf8"
+    )
     cur = conn.cursor()
     cur.execute(
         'SELECT id, name FROM states WHERE BINARY name LIKE "N%" ORDER BY id'
