f3149b34342b58b0d4621f4161f592106219d633
geraldrolland <geraldrolland123@gmail.com>
2024-06-23 05:50:59 -0700

    orm

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -3,10 +3,6 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, String, Integer
 Base = declarative_base()
 
-"""
-This scripts create the table states
-"""
-
 
 class State(Base):
     __tablename__ = "states"
